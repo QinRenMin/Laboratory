379ac16fdac0af2a62978ea81e1b401e7e5a55e6
QinRenMin <2622129594@qq.com>
2018-11-28 15:38:43 +0800

    递归

--- 二面/suanfa/resertSort.c
+++ 二面/suanfa/resertSort.c
@@ -2,14 +2,15 @@
 逆序输出数组，采用递归方式
 */
 #include<stdio.h>
-void outPut(int n) {
-    printf("%d ",n);
+void outPut(int i) {
+    if(i == 5) {
+        printf("%d ",i);
+        return;
+    }
+    outPut(i+1);
+    printf("%d ", i);
 }
 int main() {
-    int a[5] = {1,2,3,4,5} ;
-    int i = 4;
-    while(i>-1) {
-        outPut(a[i]);
-        i--;
-    }
+    outPut(1);
+    printf("\n");
 }
